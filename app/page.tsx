88649fde64244e61eafab471b4becc746bd272c9
Gregory Shklovski <gshklovs@uci.edu>
2023-07-17 17:35:10 -0700

    change title

--- app/page.tsx
+++ app/page.tsx
@@ -21,8 +21,8 @@ export default function Home() {
       <div className="lg:absolute bg-green-40 relative lg:ml-8 lg:mr-auto mx-auto max-w-2xl my-16 sm:my-28 lg:my-36 ">
         <div className="text-left">
           <h1 className="text-4xl font-montserrat font-bold tracking-tight text-amber-50  sm:text-6xl">
-            {/* Simple, Smart, and Reliable */}
-            No more nuisance. Make Home your OASIS.
+            Simple, Smart, and Reliable
+            {/* No more nuisance. Make Home your OASIS. */}
             {/* Wear your confidence */}
             {/* We&apos;d love to learn from you. Express your interest in helping us shape the future of  */}
           </h1>
